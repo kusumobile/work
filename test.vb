d7acb73aa0c887ce95d10166c78d798e1cfe0782
RyotaroKusumoto <kusumobile@gmail.com>
2019-08-25 14:04:19 +0900

    Create test.vb

---
+++ test.vb
@@ -0,0 +1,31 @@
+Sub RunSELECT()
+    Dim cn As Object, rs As Object, output As String, sql As String
+    Dim UseCase
+    UseCase = InputBox("Input use case no.", "Use Case#", "1")
+    
+    '---Connecting to the Data Source---
+    Set cn = CreateObject("ADODB.Connection")
+    With cn
+        .Provider = "Microsoft.ACE.OLEDB.12.0"
+        .ConnectionString = "Data Source=" & ThisWorkbook.Path & "\" & ThisWorkbook.Name & ";" & "Extended Properties=""Excel 12.0 Xml;HDR=YES"";"
+        .Open
+    End With
+    
+    
+    '---Run the SQL SELECT Query---
+    sql = "SELECT [Test Case ID], [Scenario], [EBJ Item#] FROM [Sheet1$] WHERE [General Scenario] = 'Item Creation' and [Use Case] = '" & UseCase & "';"
+    Set rs = cn.Execute(sql)
+    
+    Do
+       output = output & rs(0) & " ; " & rs(1) & " ; " & rs(2) & vbNewLine
+       Debug.Print rs(0); " ; " & rs(1) & " ; " & rs(2)
+       rs.Movenext
+    Loop Until rs.EOF
+    MsgBox output
+    
+    '---Clean up---
+    rs.Close
+    cn.Close
+    Set cn = Nothing
+    Set rs = Nothing
+End Sub
